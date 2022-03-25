d0c8086b485fd4c1a1a887ac9748dc365b3b7ce0
Nilay Setiya <nilaysetiya@gmail.com>
2022-03-25 23:47:51 +1300

    Fixed cd command and added help command

--- main.c
+++ main.c
@@ -4,9 +4,17 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <malloc.h>
+
+#define HIST_MAX 10
+
+static int hist_command_number = 0;
 
 void print_prompt() {
-    printf("# ");
+    printf("> ");
 }
 
 char *get_input() {
@@ -25,6 +33,8 @@ char *get_input() {
 }
 
 char **parse_input(char *line) {
+
+    // Function to parse input and return a null terminated array of tokens
 
     int buff_size = 100;
     int index = 0;
@@ -51,20 +61,38 @@ char **parse_input(char *line) {
 
 }
 
-int cd_command(char **args) {
+int cd_command(char **args, char *cwd) {
     
     if (args[1] == NULL) {
-        printf("Expected argument after cd\n");
+        // Change to home directory
+        chdir(cwd);
+
     } else {
+        // Change dir to what user specifies
         chdir(args[1]);
     }
     return 1;
 }
 
-int execute(char **args) {
+void help_command() {
+    
+    // Display help menu
+    printf("Type in a command to get started!\n");
+    printf("Use the 'exit' command to exit the shell\n");
+    printf("Remember that all comands are case sensitive\n");
+    
+}
+
+void history_handler(char **args) {
+    
+    
+}
+
+int execute_standard(char **args) {
     pid_t pid, wpid;
     int status;
 
+    // Create child process and execute command
     pid = fork();
     if (pid == 0) {
         // We are in child process
@@ -72,6 +100,7 @@ int execute(char **args) {
             printf("Didn't run correctly\n");
         }
     } else if (pid < 0) {
+        // Error forking
         printf("Fork error");
     } else {
         // Wait for parent process
@@ -81,15 +110,21 @@ int execute(char **args) {
     return 1;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
     bool RUNNING = true;
     char *line;
     char **args;
     int status = 0;
-    printf("NASH - Nilay's Shell\n");
 
+    // Get current directory
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+
+    printf("NASH - Nilay's Shell\n");
+    printf("Use the 'help' command to get started\n");
+
+    // Main loop
     while (RUNNING) {
         
         print_prompt();
@@ -98,12 +133,18 @@ int main(int argc, char **argv)
         args = parse_input(line);
 
         if (!strcmp(args[0], "cd")) {
-            printf("Running cd command\n");
-            status = cd_command(args);
-        } else if (!strcmp(tolower(args[0]), "exit")) {
+            status = cd_command(args, cwd);
+
+        } else if (!strcmp(args[0], "exit")) {
             exit(0);
+
+        } else if (!strcmp(args[0], "help")) {
+            help_command();
+
+        } else if (!strcmp(args[0], "history") || !strcmp(args[0], "h")) {
+            
         } else {
-            status  = execute(args);
+            status  = execute_standard(args);
         }
 
         free(line);
