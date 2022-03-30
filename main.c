f4855e23543ae312356503503d962cc41adac91e
Nilay Setiya <nilaysetiya@gmail.com>
2022-03-30 18:44:03 +1300

    Implemented background piping and signal handling to stop, continue, and kill jobs

--- main.c
+++ main.c
@@ -8,9 +8,13 @@
 #include <sys/wait.h>
 #include <limits.h>
 #include <malloc.h>
-
+#include <signal.h>
 
 #define HIST_MAX 10
+
+pid_t foreground;
+
+char *COMMAND;
 
 struct Job {
     int number;
@@ -92,7 +96,9 @@ int cd_command(char **args, char *cwd) {
 
     } else {
         // Change dir to what user specifies
-        chdir(args[1]);
+        if (chdir(args[1]) < 0) {
+            fprintf(stderr, "cd: %s: No such file or directory\n", args[1]);
+        }
     }
     return 1;
 }
@@ -186,12 +192,46 @@ int execute_pipe(char ***cmd) {
 
 }
 
-int execute_standard(char **args) {
+int execute_pipe_background(char ***cmd) {
+    int fildes[2];
+	pid_t pid;
+	int fdd = 0;				
+
+    // while loop to iterate through all commands
+	while (*cmd != NULL) {
+
+        // create pipe and process
+		pipe(fildes);				
+		if ((pid = fork()) == -1) {
+			perror("fork");
+			exit(1);
+		}
+		else if (pid == 0) {
+            // Child process
+			dup2(fdd, 0);
+			if (*(cmd + 1) != NULL) {
+				dup2(fildes[1], 1);
+			}
+			close(fildes[0]);
+			execvp((*cmd)[0], *cmd);
+			exit(1);
+		}
+		else { 		
+			close(fildes[1]);
+			fdd = fildes[0];
+			cmd++;
+		}
+	}
+}
+
+int execute_standard(char **args, char *line) {
     pid_t pid, wpid;
     int status;
 
     // Create child process and execute command
     pid = fork();
+    foreground = pid;
+    COMMAND = strdup(line);
     if (pid == 0) {
         // We are in child process
         if (execvp(args[0], args) == -1) {
@@ -203,7 +243,7 @@ int execute_standard(char **args) {
         printf("Fork error");
     } else {
         // Wait for parent process
-        wpid = waitpid(pid, NULL, 0);
+        wpid = waitpid(pid, NULL, WUNTRACED);
         
     }
 
@@ -218,6 +258,7 @@ pid_t execute_background(char **args, char *line) {
     pid = fork();
     jobs[job_index].id = pid;
     jobs[job_index].cmd_line = strdup(line);
+    jobs[job_index].number = job_index;
     job_index++;
     if (pid == 0) {
         // We are in child process
@@ -276,6 +317,21 @@ char *check_status(pid_t p_id) {
 
 }
 
+void sighandler(int sig_num) {
+    
+    // Reset signal to catch Ctrl-Z
+    signal(SIGTSTP, sighandler);
+    kill (foreground, SIGSTOP);
+
+    // Store job
+    jobs[job_index].id = foreground;
+    jobs[job_index].status = "stopped";
+    jobs[job_index].cmd_line = COMMAND;
+    job_index++;
+    printf("[%d] %d\n", job_index-1, foreground);
+
+}
+
 int main(int argc, char **argv) {
 
     bool RUNNING = true;
@@ -289,6 +345,8 @@ int main(int argc, char **argv) {
 
     bool background;
     pid_t pid;
+
+    signal(SIGTSTP, sighandler);
 
     // Get current directory
     char cwd[PATH_MAX];
@@ -304,7 +362,7 @@ int main(int argc, char **argv) {
         while(jobs[k].id != 0) {
             if (waitpid(jobs[k].id, NULL, WNOHANG) > 0) {
                 jobs[k].status = "Done";
-                printf("[%d] <%s> %s\n", jobs[k].id, jobs[k].status, jobs[k].cmd_line);
+                printf("[%d] <%s> %s\n", k, jobs[k].status, jobs[k].cmd_line);
                 job_tracker++;
             }
             k++;
@@ -314,6 +372,9 @@ int main(int argc, char **argv) {
         print_prompt();
 
         line = get_input();
+        if (*line == '\0') {
+            continue;
+        }
 
         hist[hist_index] = line;
         hist_index++;
@@ -333,7 +394,8 @@ int main(int argc, char **argv) {
 
             if (num_pipes) {
                 //piping
-                printf("background with piping\n");
+                execute_pipe_background(init_pipe_cmd(line));
+                num_pipes = 0;
             } else {
                 // Run normal command in the background
                 args = parse_input(line);
@@ -348,7 +410,14 @@ int main(int argc, char **argv) {
                     help_command();
 
                 } else if (!strcmp(args[0], "history") || !strcmp(args[0], "h")) {
-                    //history
+                    printf("Please execute history without the &\n");
+
+                } else if (!strcmp(args[0], "jobs")) {
+                    int j = 1;
+                    while(jobs[j].status != NULL) {
+                        printf("[%d] <%s> %s\n", j, jobs[j].status, jobs[j].cmd_line);
+                        j++;
+                    } 
                 } else {
                     // standard command
                     pid = execute_background(args, line);
@@ -359,7 +428,6 @@ int main(int argc, char **argv) {
             if (num_pipes) {
                 // piping
                 execute_pipe(init_pipe_cmd(line));
-
                 num_pipes = 0;
 
             } else {
@@ -374,38 +442,103 @@ int main(int argc, char **argv) {
                 } else if (!strcmp(args[0], "help")) {
                     help_command();
 
+                } else if (!strcmp(args[0], "fg")) {
+                    if (args[1] == NULL) {
+                        kill(foreground, SIGCONT);
+                    } else {
+                        kill(jobs[atoi(args[1])].id, SIGCONT);
+                    }
+
+                } else if (!strcmp(args[0], "bg")) {
+                    if (args[1] == NULL) {
+                        kill(jobs[job_index-1].id, SIGCONT);
+                    } else {
+                        kill(jobs[atoi(args[1])].id, SIGCONT);
+                    }
+                } else if (!strcmp(args[0], "kill")) {
+                    if (args[1] == NULL) {
+                        kill(foreground, SIGKILL);
+                    } else {
+                        kill(jobs[atoi(args[1])].id, SIGKILL);
+                    }
+
                 } else if (!strcmp(args[0], "jobs")) {
                     int j = 1;
                     while(jobs[j].status != NULL) {
-                        printf("[%d] <%s> %s\n", jobs[j].id, jobs[j].status, jobs[j].cmd_line);
+                        printf("[%d] <%s> %s\n", j, jobs[j].status, jobs[j].cmd_line);
                         j++;
                     }
 
                 } else if (!strcmp(args[0], "history") || !strcmp(args[0], "h")) {
                     if (history_handler(hist, hist_index, args)) {
-                        char *hist_line;
-                        char **hist_args;
+                        char *hist_line = hist[atoi(args[1])-1];
+                        char **hist_args = parse_input(hist_line);
+                        int hist_pipes = 0;
+                        bool hist_background = false;
 
-                        hist_line = hist[atoi(args[1])-1];
-                        hist_args = parse_input(hist_line);
+                        for (int i = 0; hist_line[i] != '\0'; i++) {
+                            if (hist_line[i] == '|' && hist_line[i-1] != '|' && hist_line[i+1] != '|') {
+                                hist_pipes++;
+                            }
 
-                        if (!strcmp(hist_args[0], "cd")) {
-                            status = cd_command(hist_args, cwd);
+                            if (line[i] == '&' && line[i-1] != '&' && line[i+1] != '&') {
+                                hist_background = true;
+                            }
+                        }
 
-                        } else if (!strcmp(hist_args[0], "exit")) {
-                            exit(0);
+                        if (hist_background) {
+                            
+                            if (hist_pipes) {
+                                // hist background piping
+                                hist_line = hist[atoi(args[1]-1)];
+                                execute_pipe_background(init_pipe_cmd(hist_line));
+                            } else {
 
-                        } else if (!strcmp(hist_args[0], "help")) {
-                            help_command();
+                                hist_line = hist[atoi(args[1])-1];
+                                hist_args = parse_input(hist_line);
 
+                                if (!strcmp(hist_args[0], "cd")) {
+                                    status = cd_command(hist_args, cwd);
+
+                                } else if (!strcmp(hist_args[0], "exit")) {
+                                    exit(0);
+
+                                } else if (!strcmp(hist_args[0], "help")) {
+                                    help_command();
+
+                                } else {
+                                    execute_background(hist_args, hist_line);
+                                }
+                            }
                         } else {
-                            status = execute_standard(hist_args);
+
+                            if (hist_pipes) {
+                                hist_line = hist[atoi(args[1]-1)];
+                                execute_pipe(init_pipe_cmd(hist_line));
+                            } else {
+
+                                hist_line = hist[atoi(args[1])-1];
+                                hist_args = parse_input(hist_line);
+
+                                if (!strcmp(hist_args[0], "cd")) {
+                                    status = cd_command(hist_args, cwd);
+
+                                } else if (!strcmp(hist_args[0], "exit")) {
+                                    exit(0);
+
+                                } else if (!strcmp(hist_args[0], "help")) {
+                                    help_command();
+
+                                } else {
+                                    status = execute_standard(hist_args, hist_line);
+                                }
+                            }
                         }
 
                     }
 
                 } else {
-                    status  = execute_standard(args);
+                    status  = execute_standard(args, line);
                 }
 
                 free(args);
